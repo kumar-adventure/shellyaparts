0ce139c1e3b81652dcd64705bfee38f283949048
kumar <kumar.adventure@gmail.com>
2015-06-01 11:30:38 +0530

    write code for select plan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  protected
+
+  def check_subscription
+  	if current_user
+  	  unless current_user.subscriptions.last.is_active
+  	  	redirect_to plans_path, alert: "Please choose a plan Firstly  !!!!"
+  	  end
+  	end
+  end
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  before_action :check_subscription
+  
   def index
   end
+
 end
