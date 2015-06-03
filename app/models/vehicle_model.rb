61d6b8136860243789b5d69b1e40128f5ed20645
kumar <kumar.adventure@gmail.com>
2015-06-03 14:09:25 +0530

    apply validation for models

--- app/models/vehicle_model.rb
+++ app/models/vehicle_model.rb
@@ -1,5 +1,9 @@
 class VehicleModel < ActiveRecord::Base
   acts_as_paranoid
+
+  validates_presence_of :name, :vehicle_make_id
+  validates_uniqueness_of :name
+
   has_many :vehicles
   belongs_to :vehicle_make
 
